f697a01b23f3aa7ac2b3700c593bba512c40a83b
Angela Salomão <angelasalomaokl@gmail.com>
2022-06-22 18:45:56 -0300

    adicao do banner com informações

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Banner } from './components/Banner/Banner';
 import { Header } from './components/Header/Header';
 
 function App() {
   return (
-   <Header />
+    <>
+      <Header />
+      <Banner />
+   </>
   );
 }
 
