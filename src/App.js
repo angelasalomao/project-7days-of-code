46da26fde7550693da0daf0a578879d1cb349247
Angela Salomão <angelasalomaokl@gmail.com>
2022-06-24 18:48:42 -0300

    adicionado secao de metricas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import { Header } from './components/Header/Header';
 import { Banner } from './components/Banner/Banner';
+import { Metricas } from './components/Metricas/Metricas';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <main>
       <Header />
       <Banner />
-   </>
+      <div className="metricas-container">
+        <Metricas
+          quantity="400+"
+          title="Projetos concluídos"
+          info="Nós ajudamos a construir mais de 400 projetos incríveis."
+        />
+        <Metricas
+          quantity="100+"
+          title="Colaboradores"
+          info="Temos mais de 100 colaboradores no nosso time que se preocupam com nossos clientes."
+        />
+        <Metricas
+          quantity="20k"
+          title="Downloads"
+          info="Nossos projetos que estão disponíveis em lojas já foram baixados mais de 20.000 vezes."
+        />
+        <Metricas
+          quantity="500+"
+          title="Reviews 5 estrelas"
+          info="Estamos orgulhosos de contar com mais de 500 reviews 5 estrelas em nossos produtos."
+        />
+      </div>
+   </main>
   );
 }
 
