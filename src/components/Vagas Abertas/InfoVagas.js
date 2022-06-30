799b9d693a56e7c98889398745bfe52a65a82df2
Angela Salomão <angelasalomaokl@gmail.com>
2022-06-30 17:32:11 -0300

    correção de tag pro título

--- src/components/Vagas Abertas/InfoVagas.js
+++ src/components/Vagas Abertas/InfoVagas.js
@@ -5,7 +5,7 @@ export const InfoVagas = ({ office, hours, remuneration }) => {
   return (
     <article className="work-details">
       <div className="about-details">
-        <span className="work-name">{office}</span>
+        <h1 className="work-name">{office}</h1>
         <div>
           <span className="office-hours">{hours}</span>
           <span className="work-remuneration">{remuneration}</span>
